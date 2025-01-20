da85715e6258f6111597b2a08bc7a734076db7b7
Karol Pietrów <karolpietrow2004@gmail.com>
2025-01-20 18:43:29 +0100

    v.0.4.1

--- app/src/main/java/pl/karolpietrow/kp8/BookRepository.kt
+++ app/src/main/java/pl/karolpietrow/kp8/BookRepository.kt
@@ -5,4 +5,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 class BookRepository {
     private val db = FirebaseFirestore.getInstance()
     private val collection = db.collection("books")
+
+//    suspend fun addBook(book: BookModel) {
+//
+//    }
 }
--- app/src/main/java/pl/karolpietrow/kp8/MainActivity.kt
+++ app/src/main/java/pl/karolpietrow/kp8/MainActivity.kt
@@ -14,6 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
@@ -32,6 +35,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
 import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import pl.karolpietrow.kp8.api.NetworkResponse
@@ -54,14 +58,14 @@ fun BookScreen() {
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
     val bookViewModel = BookViewModel()
-    val searchStatus = bookViewModel.searchStatus.observeAsState()
 
     var input by remember { mutableStateOf("") }
     var isLoading by remember { mutableStateOf(false) }
-
     var bookList by remember { mutableStateOf(listOf<BookModel>()) }
+    var selectedBook by remember { mutableStateOf<BookModel?>(null) }
 
-    fun setData(bookResult: NetworkResponse<BookModel>) {
+
+    bookViewModel.bookStatus.observe(lifecycleOwner) { bookResult ->
         when (bookResult) {
             is NetworkResponse.Error -> {
                 Toast.makeText(context, bookResult.message, Toast.LENGTH_SHORT).show()
@@ -80,8 +84,107 @@ fun BookScreen() {
         }
     }
 
-    bookViewModel.bookStatus.observe(lifecycleOwner) { response ->
-        setData(response)
+    bookViewModel.searchStatus.observe(lifecycleOwner) { searchResult ->
+        when (searchResult) {
+            is NetworkResponse.Error -> {
+                Toast.makeText(context, searchResult.message, Toast.LENGTH_SHORT).show()
+                isLoading = false
+            }
+            is NetworkResponse.Loading -> {
+                isLoading = true
+            }
+            is NetworkResponse.Success -> {
+                if (searchResult.data.toString().isNotEmpty()) {
+                    if (searchResult.data.count > 1) {
+                        val id: Int = searchResult.data.results[0].id
+                        bookViewModel.getBookByID(id.toString())
+                    } else if (searchResult.data.count == 1) {
+                        val id: Int = searchResult.data.results[0].id
+                        bookViewModel.getBookByID(id.toString())
+                    }
+                } else {
+                    Toast.makeText(context, "Błąd ładowania danych", Toast.LENGTH_SHORT).show()
+                }
+                isLoading = false
+            }
+            null -> {}
+        }
+    }
+
+    if (selectedBook != null) {
+        AlertDialog(
+            modifier = Modifier.fillMaxWidth(),
+            onDismissRequest = { selectedBook = null },
+            confirmButton = {
+                Button(onClick = { selectedBook = null }) {
+                    Text("Zamknij")
+                }
+            },
+            title = {
+                Text(text = selectedBook!!.title)
+            },
+            text = {
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = "ID: " +  selectedBook!!.id,
+                        fontSize = 17.sp
+                    )
+                    Text(
+                        modifier = Modifier.padding(5.dp),
+                        text = "Tytuł: " + selectedBook!!.title,
+                        fontSize = 17.sp,
+                    )
+                    if (selectedBook!!.authors.isNotEmpty()) {
+                        Text(
+                            modifier = Modifier.padding(5.dp),
+                            text = "Autorzy: ",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        selectedBook!!.authors.forEach { author ->
+                            Text(author.name, modifier = Modifier.padding(5.dp))
+                        }
+                    }
+                    if (selectedBook!!.subjects.isNotEmpty()) {
+                        Text(
+                            modifier = Modifier.padding(5.dp),
+                            text = "Tematyka: ",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        selectedBook!!.subjects.forEach { subject ->
+                            Text(subject, modifier = Modifier.padding(5.dp))
+                        }
+                    }
+                    if (selectedBook!!.languages.isNotEmpty()) {
+                        Text(
+                            modifier = Modifier.padding(5.dp),
+                            text = "Języki: ",
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        selectedBook!!.languages.forEach { lang ->
+                            Text(lang, modifier = Modifier.padding(5.dp))
+                        }
+                    }
+                    Button(
+                        onClick = {
+
+                        },
+                        modifier = Modifier.padding(5.dp)
+                    ) {
+                        Text("Załaduj treść książki")
+                    }
+                }
+            },
+            properties = DialogProperties(
+                usePlatformDefaultWidth = false // Umożliwia pełną szerokość
+            )
+        )
     }
 
     Column(
@@ -128,7 +231,7 @@ fun BookScreen() {
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(10.dp)
-//                            .clickable { selectedBook = book }
+                            .clickable { selectedBook = book }
                     ) {
                         Text(
                             modifier = Modifier.padding(5.dp),
@@ -173,22 +276,15 @@ fun BookScreen() {
             Row {
                 Button(
                     onClick = {
-                        if (!input.isEmpty()) {
+                        if (input.isNotEmpty()) {
                             if (input.isDigitsOnly()) {
                                 bookViewModel.getBookByID(input)
                             } else {
-
+                                bookViewModel.getBookBySearch(input)
                             }
                         } else {
                             Toast.makeText(context, "Pole nie może być puste!", Toast.LENGTH_SHORT).show()
                         }
-
-//                    } else {
-//                        val intent = Intent(context, BookService::class.java).apply {
-//                            putExtra("minID", minID.toInt())
-//                            putExtra("maxID", maxID.toInt())
-//                        }
-//                        context.startService(intent)
                     },
                     modifier = Modifier.padding(5.dp)
                 ) {
@@ -196,7 +292,7 @@ fun BookScreen() {
                 }
                 Button(
                     onClick = {
-//                    viewModel.clearList()
+                        bookList = emptyList()
                     },
                     modifier = Modifier.padding(5.dp)
                 ) {
